ad8fb68a9aaa70212e8d68240b6d78861a66d518
SergeySerkov <sergeyserkov01@gmail.com>
2021-02-21 17:55:56 +0300

    Updates

---
+++ precache-manifest.4e458cd846fae0e8a82ac46f1d76626c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93ee3208d67dd9f5e9b1cb825d4b326",
+    "url": "/GetPizza/index.html"
+  },
+  {
+    "revision": "74fb61f1fd5a07520591",
+    "url": "/GetPizza/static/css/main.2863f604.chunk.css"
+  },
+  {
+    "revision": "80ee4aeed95bb357b51e",
+    "url": "/GetPizza/static/js/2.aa9bdc2e.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/GetPizza/static/js/2.aa9bdc2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74fb61f1fd5a07520591",
+    "url": "/GetPizza/static/js/main.c9682b35.chunk.js"
+  },
+  {
+    "revision": "556d84ccb6af3d14ea31",
+    "url": "/GetPizza/static/js/runtime-main.872cb99e.js"
+  },
+  {
+    "revision": "4a874d2e3dec47c9e89d32f92f84af5b",
+    "url": "/GetPizza/static/media/bg.4a874d2e.webp"
+  }
+]);
